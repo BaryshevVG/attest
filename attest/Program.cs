9acf54d989339e79e13c03e2bf0b44193e1146dc
Vadim Baryshev <vadik.barysheff@gmail.com>
2024-03-13 20:52:29 +0500

    add answer:task2

--- attest/Program.cs
+++ attest/Program.cs
@@ -15,3 +15,18 @@ void PrintNum(int number2)
 Console.WriteLine("Введите число N: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 PrintNum(number2);
+
+// Задание 2
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AkkermanFunction(m - 1, 1);
+    }
+    return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+}
