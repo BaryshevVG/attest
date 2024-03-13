9e3cb63304a402ef50f0e023cd76473ac18fb50d
Vadim Baryshev <vadik.barysheff@gmail.com>
2024-03-13 20:50:35 +0500

    add naswer:task1

---
+++ attest/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задание 1.
+
+Console.WriteLine("Введите число M: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+void PrintNum(int number2)
+{
+    if (number1 > number2)
+    {
+        return;
+    }
+    PrintNum(number2 - 1);
+    Console.Write($"{number2} ");
+}
+
+Console.WriteLine("Введите число N: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+PrintNum(number2);
